c356eb7f30c622708ef9fef8efa71ae1a3de16de
John M <jkmancuso@gmail.com>
2024-09-08 20:23:00 -0400

    parse influx output for time

--- dags/analyzetraffic.py
+++ dags/analyzetraffic.py
@@ -7,10 +7,32 @@ from datetime import datetime
 org = "personal"
 bucket = "network"
 start = "-30d"
+drop = '"_start","_stop","_value","_field","_measurement"'
+expected_field_num = 7
+
+query = f"""from(bucket: \"{bucket}\") |> 
+        range(start: {start}) |>
+        drop (columns: [{drop}])"""
+
 
 def pull_data(ti):
-    val=ti.xcom_pull(task_ids="query_influx_task")
-    print(val)
+    val=str(ti.xcom_pull(task_ids="query_influx_task"))
+
+    x = []
+
+    #['', 'result', 'table', '_time', 'dest_ip', 'source_dns', 'source_ip']
+    for i, line in enumerate(val.splitlines()):
+        line_list = line.split(",")
+        
+        if i == 0 or len(line_list) != expected_field_num:
+            continue
+        
+        hour = datetime.fromisoformat(line_list[3]).hour
+
+        x.append(hour)
+
+    print(x)
+        
 
 with DAG(
     "my-dag", start_date=datetime(2024,9,7),
@@ -22,7 +44,7 @@ with DAG(
         http_conn_id="influx_https",
         endpoint=f"/api/v2/query?org={org}",
         method="POST",
-        data=f"from(bucket: \"{bucket}\")|> range(start: {start})",
+        data=query
     )
     python_task = PythonOperator(
         task_id="python_task",
